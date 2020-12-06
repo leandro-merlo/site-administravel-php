6d88246afdfb0e4eb3782ced7c93345a03687375
Leandro Manzano Merlo <leandro.merlo@gmail.com>
2020-12-06 10:08:21 -0300

    Adicionando o Bootstrap e o JQuery no projeto; configurando o template admin

--- templates/admin.tpl.php
+++ templates/admin.tpl.php
@@ -1,12 +1,19 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Painel Administrativo</title>
-</head>
-<body>
-    <h1>Administração</h1>
-    <?php require_once $content; ?>
-</body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css" >
+
+    <title>Paine Administrativo</title>
+  </head>
+  <body>
+    <?php include $content ?>
+
+    <script src="/js/jquery.slim.min.js"></script>
+    <script src="/js/bootstrap.bundle.min.js" ></script>
+  </body>
 </html>
