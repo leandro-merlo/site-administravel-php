c94b8de5d6fec8f6a9622cd179b7af151c0d1839
Leandro Manzano Merlo <leandro.merlo@gmail.com>
2020-12-06 18:32:05 -0300

    Estilização do template admin

--- templates/admin.tpl.php
+++ templates/admin.tpl.php
@@ -8,10 +8,40 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="/css/bootstrap.min.css" >
 
+    <link rel="stylesheet" href="/css/style.css" >
+
     <title>Paine Administrativo</title>
   </head>
-  <body>
-    <?php include $content ?>
+  <body class="d-flex flex-column">
+    <div id="header">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a href="" class="navbar-brand">Admin</a>
+            <span class="navbar-text">Painel Administrativo</span>
+        </nav>
+    </div>
+    <div id="main">
+        <div class="row">
+            <div class="col">
+                <ul id="main-menu" class="nav flex-column nav-pills bg-secondary text-white p2">
+                    <li class="nav-item">
+                        <span class="nav-link text-white-50"><small>MENU</small></span>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/" class="nav-link active">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/users" class="nav-link">Usuários</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/pages" class="nav-link">Páginas</a>
+                    </li>                    
+                </ul>
+            </div>
+            <div id="content" class="col-10">
+                <?php include $content; ?>            
+            </div>
+        </div>
+    </div>
 
     <script src="/js/jquery.slim.min.js"></script>
     <script src="/js/bootstrap.bundle.min.js" ></script>
