49277e8e9669be28769ac1b21f27eef98f83256c
Leandro Manzano Merlo <leandro.merlo@gmail.com>
2020-12-07 21:46:02 -0300

    Criação da página de visualização das páginas

--- templates/admin/pages/index.tpl.php
+++ templates/admin/pages/index.tpl.php
@@ -12,7 +12,7 @@
         <tr>
             <td>1</td>
             <td><a href="/admin/pages/1">Página Inicial</a></td>
-            <td class="text-right"><a href="/admin/pages/1/edit" class="btn btn-primary btn-sm">ver</a></td>
+            <td class="text-right"><a href="/admin/pages/1" class="btn btn-primary btn-sm">ver</a></td>
         </tr>
     </tbody>
 </table>
--- templates/admin/pages/view.tpl.php
+++ templates/admin/pages/view.tpl.php
@@ -1,5 +1,28 @@
 <h3>Administração de Páginas</h3>
 
-// Visualização
+<div class="row">
+    <div class="col-3">
+        <dl class="row">
+            <dt class="col-sm-4">Título</dt>
+            <dd class="col-sm-8">Página inicial</dd>
 
-<a href="/admin">Home</a>
+            <dt class="col-sm-4">URL</dt>
+            <dd class="col-sm-8">/ - <a href="/" target="_blank">abrir</a></dd>
+
+            <dt class="col-sm-4">Criado em:</dt>
+            <dd class="col-sm-8">2020-01-01 01:01</dd>
+
+            <dt class="col-sm-4">Atualizado em:</dt>
+            <dd class="col-sm-8">2020-01-01 01:01</dd>
+        </dl>
+    </div>
+    <div class="col bg-light">
+        <h3>Página Inicial</h3>
+        <p>Esta é a página inicial do site</p>
+    </div>
+</div>
+<p>
+    <a href="/admin/page/1/edit" class="btn btn-primary">Editar</a>
+    <a href="/admin/page/1/delete" class="btn btn-danger">Remover</a>
+</p>
+<a class="btn btn-secondary" href="/admin/pages">Voltar</a>
