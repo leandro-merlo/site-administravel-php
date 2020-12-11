801eed437a56eeb787a6378e234d174c6fe4e74e
Leandro Manzano Merlo <leandro.merlo@gmail.com>
2020-12-10 23:01:09 -0300

    Adicionado o dump do banco de dados inicial

---
+++ dump.sql
@@ -0,0 +1,54 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema pp_criando_um_site_com_php
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema pp_criando_um_site_com_php
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `pp_criando_um_site_com_php` DEFAULT CHARACTER SET utf8 ;
+USE `pp_criando_um_site_com_php` ;
+
+-- -----------------------------------------------------
+-- Table `pp_criando_um_site_com_php`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pp_criando_um_site_com_php`.`users` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(120) NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `pp_criando_um_site_com_php`.`pages`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pp_criando_um_site_com_php`.`pages` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(255) NOT NULL,
+  `url` VARCHAR(255) NOT NULL,
+  `body` LONGTEXT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
+  `users_id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_pages_users_idx` (`users_id` ASC) VISIBLE,
+  CONSTRAINT `fk_pages_users`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `pp_criando_um_site_com_php`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
