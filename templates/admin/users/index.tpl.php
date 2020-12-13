d6a92ab9517fbb42de973a8a47b3a5c8f36f6da1
Leandro Manzano Merlo <leandro.merlo@gmail.com>
2020-12-13 16:42:06 -0300

    Criaçao do template da listagem de usuarios e correçao na listagem de paginas, reparando a classe da tabela e adicionando uma mensagem quando nao existem registros a serem exibidos

--- templates/admin/users/index.tpl.php
+++ templates/admin/users/index.tpl.php
@@ -1,20 +1,20 @@
-<h3 class="mb-5">Administração de Páginas</h3>
+<h3 class="mb-5">Administração de Usuários</h3>
 
 <table class="table table-hover table-stripped">
     <thead>
         <tr>
             <th>#</th>
-            <th>Título</th>
+            <th>email</th>
             <th></th>
         </tr>
     </thead>
     <tbody>
-        <?php foreach($data['pages'] as $page): ?>
+        <?php foreach($data['users'] as $user): ?>
         <tr>
-            <?php $id = $page['id'] ?>
+            <?php $id = $user['id'] ?>
             <td><?= $id; ?></td>
-            <td><a href="/admin/pages/<?= $id; ?>"><?= $page['title']; ?></a></td>
-            <td class="text-right"><a href="/admin/pages/<?= $id; ?>" class="btn btn-primary btn-sm">ver</a></td>
+            <td><a href="/admin/users/<?= $id; ?>"><?= $user['email']; ?></a></td>
+            <td class="text-right"><a href="/admin/users/<?= $id; ?>" class="btn btn-primary btn-sm">ver</a></td>
         </tr>
         <?php endforeach; ?>
         <?php if (isset($data['users']) and count($data['users']) <= 0) :?>
@@ -26,4 +26,4 @@
 </table>
 
 
-<a class="btn btn-secondary" href="/admin/pages/create">Novo</a>
+<a class="btn btn-secondary" href="/admin/users/create">Novo</a>
