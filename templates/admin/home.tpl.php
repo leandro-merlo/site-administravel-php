7626c91a8548e3a9596216473835005e3d086b82
Leandro Manzano Merlo <leandro.merlo@gmail.com>
2020-12-16 20:36:59 -0300

    Reparo no estilo dos botoes da Home / Admin

--- templates/admin/home.tpl.php
+++ templates/admin/home.tpl.php
@@ -2,5 +2,5 @@
 
 <p>O que você deseja fazer?</p>
 
-<a href="/admin/users" class="btn btn-success btn-large pt-5 pb-5"><i class="fa fa-user" style="font-size: 70px"></i><br />Administração de Usuários</a>
-<a href="/admin/pages" class="btn btn-primary btn-large  pt-5 pb-5"><i class="fa fa-file-text-o" style="font-size: 70px"></i><br />Administração de Páginas</a>
+<a href="/admin/users" class="btn btn-success btn-large "><i class="fa fa-user m-2" style="font-size: 70px"></i><br />Gerenciar Usuários</a>
+<a href="/admin/pages" class="btn btn-primary btn-large  "><i class="fa fa-file-text-o m-2" style="font-size: 70px"></i><br />Gerenciar Páginas</a>
